4195ac543779e88159ae647f5fe074c18931a5d7
ta-jana <tatyrkova.jana@email.cz>
2022-12-02 17:36:18 +0100

    adding word class file

--- app/src/main/java/com/example/roomwordsample/Word.kt
+++ app/src/main/java/com/example/roomwordsample/Word.kt
@@ -6,4 +6,9 @@ import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "word_table")
-class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "word") val word: String
+
+)
